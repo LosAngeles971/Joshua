a831f1f1d2700e0035db1d9d9358753bbd4f6063
LosAngeles971 <LosAngeles1791@gmail.com>
2021-02-11 00:15:03 +0100

    genetic

--- go/internal/genetic/genetic.go
+++ go/internal/genetic/genetic.go
@@ -1,64 +1,52 @@
 package genetic
 
 import (
-    "math/rand"
-    "it/losangeles971/joshua/internal/runtime"
-    "it/losangeles971/joshua/internal/knowledge"
-    ctx "it/losangeles971/joshua/internal/context"
+	"fmt"
+	ctx "it/losangeles971/joshua/internal/context"
+	"it/losangeles971/joshua/internal/knowledge"
+	"it/losangeles971/joshua/internal/runtime"
+	"math/rand"
 )
-
-type Openrange struct {
-    Min     float64
-    Max     float64
-}
-
-func (g Openrange) Get() float64 {
-    return g.Min + rand.Float64() * (g.Max - g.Min)
-}
-
-type Set struct {
-    Values []float64
-}
-
-func (g Set) Get() float64 {
-    return g.Values[rand.Intn(len(g.Values))]
-}
-
-type Gene interface {
-    Get() float64
-}
 
 type Person struct {
     DNA         map[string]float64
-    Fitness     float64
+    locked      map[string]bool
+    ranking     float64
+}
+
+func (p *Person) clone() *Person {
+    pp := Person{
+        DNA: p.DNA,
+        locked: p.locked,
+        ranking: p.ranking,
+    }
+    return &pp
 }
 
 type Population struct  {
     Fitness             runtime.Path
     Size                int
-    GeneticCode         map[string]Gene
-    Population          []Person
+    GeneticCode         ctx.State
+    Population          []*Person
     Selective_pressure  float64     // usually between 1 and 2, greater value privileges good item for recombination
     Selection_ratio     float64     // usually 0.5, the size (%) of population to be replaced
     Mutation_rate       float64     // the probability of having one gene mutated inside a child
 }
 
-func (p *Population) AddGene(key string, g Gene) {
-    p.GeneticCode[key] = g
-}
-
-func (p Population) RandomPerson() Person {
+func (p Population) RandomPerson() *Person {
     return p.Population[rand.Intn(len(p.Population))]
 }
 
 func (p *Population) Init() {
-    p.Population = []Person{}
+    p.Population = []*Person{}
     for i := 0; i < p.Size; i++ {
-        person := Person{DNA: map[string]float64{}}
-        for key, gene := range p.GeneticCode {
-            person.DNA[key] = gene.Get()
+        person := Person{DNA: map[string]float64{}, locked: map[string]bool{}}
+        for _, key := range p.GeneticCode.Keys() {
+            v, _ := p.GeneticCode.Get(key)
+            person.DNA[key] = v.GetRandom()
+            person.locked[key] = false
         }
-        p.Population = append(p.Population, person)
+        p.Population = append(p.Population, &person)
     }
 }
 
@@ -69,21 +57,32 @@ func (p *Population) ranking() error {
     for _, person := range p.Population {
         state := ctx.Create()
         for k, v := range person.DNA {
-            state.Add(k, v)
+            state.Update(k, v)
         }
+        p.Fitness.Reset()
         err := p.Fitness.Run(state, 0)
         if err != nil {
             return err
         }
+        fmt.Println("Outcome: ", p.Fitness.Outcome())
         switch p.Fitness.Outcome() {
         case knowledge.CE_OUTCOME_TRUE:
-            person.Fitness = 1.0
+            for k := range person.DNA {
+                person.locked[k] = true
+            }
+            person.ranking = 1.0
         case knowledge.CE_OUTCOME_EFFECT_FALSE:
-            person.Fitness = 0.75
+            person.ranking = 0.75
         case knowledge.CE_OUTCOME_CAUSE_FALSE:
-            person.Fitness = 0.25
+            person.ranking = 0.25
+            for k := range person.DNA {
+                person.locked[k] = false
+            }
         default:
-            person.Fitness = 0
+            person.ranking = 0
+            for k := range person.DNA {
+                person.locked[k] = false
+            }
         }
     }
     return nil
@@ -102,11 +101,11 @@ https://en.wikipedia.org/wiki/Fitness_proportionate_selection
 func (p Population) choose() int {
     total := 0.0
     for _, i := range p.Population {
-        total += i.Fitness
+        total += i.ranking
     }
     value := rand.Float64() * total
     for ix, i := range p.Population {
-        value -= i.Fitness
+        value -= i.ranking
         if value <= 0 {
             return ix
         }
@@ -115,48 +114,61 @@ func (p Population) choose() int {
 }
 
 func (p *Population) selection() {
-    alpha := []Person{}
-    for n := 0; n < int(p.Selection_ratio * float64(len(p.Population))); n++ {
+    alpha := []*Person{}
+    selected := int(p.Selection_ratio * float64(len(p.Population)))
+    for n := 0; n < selected; n++ {
         ix := p.choose()
-        alpha = append(alpha, p.Population[ix])
+        alpha = append(alpha, p.Population[ix].clone())
         p.kill(ix)
     }
     p.Population = alpha
  }
 
 func (p Population) mutate(child *Person) {
-    // an approach to randomly select a key of the DNA
-    r := rand.Intn(len(child.DNA))
-	for k := range child.DNA {
-		if r == 0 {
-			child.DNA[k] = p.GeneticCode[k].Get()
-            return
-		}
-		r--
-	}
-	panic("unreachable")
+    unlocked := []string{}
+    for k := range child.DNA {
+        if !child.locked[k] {
+            unlocked = append(unlocked, k)
+        }
+    }
+    if len(unlocked) == 0 {
+        return
+    }
+    r := rand.Intn(len(unlocked))
+	v, _ := p.GeneticCode.Get(unlocked[r])
+	child.DNA[unlocked[r]] = v.GetRandom()
 }
 
-func (p Population) combine(mother Person, father Person) Person {
+func (p Population) combine(mother *Person, father *Person) *Person {
     child := Person{}
     child.DNA = map[string]float64{} 
+    child.locked = map[string]bool{}
     for key := range mother.DNA {
-        r := rand.Float64()
-        if r < 0.5 {
+        if mother.locked[key] {
             child.DNA[key] = mother.DNA[key]
-        } else {
+            child.locked[key] = true
+        } else if father.locked[key] {
             child.DNA[key] = father.DNA[key]
+            child.locked[key] = true
+        } else {
+            child.locked[key] = false
+            r := rand.Float64()
+            if r < 0.5 {
+                child.DNA[key] = mother.DNA[key]
+            } else {
+                child.DNA[key] = father.DNA[key]
+            }
         }
     }
     r := rand.Float64()
     if r < p.Mutation_rate {
         p.mutate(&child)
     }
-    return child
+    return &child
 }
 
 func (p *Population) crossover() {
-    childs := []Person{}
+    childs := []*Person{}
     for (len(p.Population) + len(childs)) < p.Size {
         mother := p.RandomPerson()
         father := p.RandomPerson()
@@ -165,11 +177,15 @@ func (p *Population) crossover() {
     p.Population = append(p.Population, childs...)
 }
 
-func Cycle(p *Population, generations int) {
+func Cycle(p *Population, generations int) error {
     p.Init()
     for g := 0; g < generations; g++ {
-        p.ranking()
+        err := p.ranking()
+        if err != nil {
+            return err
+        }
         p.selection()
         p.crossover()
     }
+    return nil
 }
